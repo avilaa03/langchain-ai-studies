c4032b56029ae0660f839afc8de4bf0905f41447
avilaa03 <lucasdeavila2003@gmail.com>
2024-11-07 15:48:19 -0300

    Created Tavily Search API to locate the LinkedIn profile by the name

--- agents/linkedin_lookup_agent.py
+++ agents/linkedin_lookup_agent.py
@@ -1,7 +1,8 @@
 import os
+import sys
 from dotenv import load_dotenv
-load_dotenv()
 from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 from langchain_core.prompts import PromptTemplate
 from langchain_core.tools import Tool
 from langchain.agents import (
@@ -9,28 +10,46 @@ from langchain.agents import (
     AgentExecutor
 )
 from langchain import hub
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from tools.tools import get_profile_url_tavily
+
+load_dotenv()
 
 
 def lookup(name: str) -> str:
-    llm = ChatOpenAI(
-        temperature=0,
-        model_name="gpt-4o-mini"
-    )
+    # llm = ChatOpenAI(
+    #     temperature=0,
+    #     model_name="gpt-4o-mini"
+    # )
+    llm = ChatOllama(model="llama3")
+
     template = """given the full name {name_of_person} I want you to get it me a link of their LinkedIn profile page.
     Your answer should contain only a URL"""
-    
+
     prompt_template = PromptTemplate(
         template=template, input_variables=["name_of_person"]
     )
+
     tools_for_agent = [
         Tool(
-            name="Crawl Goolge 4 linkedin profile page",
-            func="?",
+            name="Crawl Google 4 linkedin profile page",
+            func=get_profile_url_tavily,
             description="useful for when you need get the LinkedIn Page URL"
         )
     ]
-    return "https://www.linkedin.com/in/lucasdeavila"
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input={"input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linkedin_profile_url = result("output")
+
+    return linkedin_profile_url
 
 if __name__ == "__main__":
-    linkedin_url = lookup("lucasdeavila")
+    linkedin_url = lookup("Lucas de Ávila Moreira Linkedin profile")
     print(linkedin_url)
